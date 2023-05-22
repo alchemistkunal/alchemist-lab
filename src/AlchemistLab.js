5bc3031a24de328317bd76656addb2980e52e179
alchemistk <alchemisttech+github@pm.me>
2023-05-22 20:09:30 +0800

    enhancing mobile/small screen views and updating configs

--- src/AlchemistLab.js
+++ src/AlchemistLab.js
@@ -1,11 +1,13 @@
-import React, { useState, useEffect, useRef }  from 'react';
+import React, { useState, useEffect }  from 'react';
 import './AlchemistLab.css';
 import profileImage from './resources/portrait.jpg'; // Replace with the actual path to your profile image
 import Project from './Project'; // Import the Project component
-import axios from 'axios';
 import './logo.css'
+import fetchData from './fetch_content'
+import axios from 'axios';
 
 import { config } from './resources/config/config.js';
+import fetchHeadlineData from './fetch_content';
 
 function importAll(r) {
   return r.keys().map(r);
@@ -22,40 +24,47 @@ function AlchemistLab() {
   const [projects, setProjects] = useState([]);
   const [headline, setHeadline] = useState("");
   const [intro, setIntro] = useState("");
+
+  
   const [refresh, setRefresh] = useState(false);
   useEffect(() => {
     // Fetch the list of strings from the API
     setRefresh(true);
-    axios.get(apiUrl+'/portfolio/profile')
+
+    axios.get(apiUrl)
       .then(response => {
-        // console.log(response.data); // Check the response data
+
         setProjects(response.data.projects);
         setHeadline(response.data.gold_dust);
         setIntro(response.data.about_me);
       })
       .catch(error => {
         console.error(error);
-      }).finally(()=> setRefresh(false));
-     
-      
+      }).finally(()=> {
+        setRefresh(false);
+        
+
+      });
+    
+
   }, []);
 
   
   return (
     <div className="container">
-      <header>
+        <header>        
         <h1>Kunal's Portfolio</h1>
       </header>
       <main>
       <div className="one-liner">
-        <h1>{headline}</h1>
+        <h1>{refresh  ? "loading..." : headline}</h1>
       </div>
 
         <section id="about">
           <div className="about-content">
             <div className="about-text">
               <h2>About me</h2>
-              <p>{intro}</p>
+              <p>{refresh  ? "loading..." : intro}</p>
 
             </div>
             <div className="about-image">
@@ -65,31 +74,22 @@ function AlchemistLab() {
 
         </section>
         <section id="portfolio" className='portfolio-container'>
-        {/* <div class="portfolio-conta">
-        </div> */}
-          <h2>Portfolio</h2><br/>
-          {/* <p>Here you can find some examples of my work and projects I've completed.</p> */}
+
+          <h2>Portfolio {refresh? "(loading...)":""}</h2><br/>
+          
           <ul className="portfolio-list">
+            {/* intentially added to get a diveder above the 1st project */}
             <li></li> 
 
-            {projects.map((project, index) => (
+            {!refresh  ? projects.map((project, index) => (
               <Project key={index} project={project}/>
-            ))}
+            )): ""}
           </ul>
 
         </section>
-        {/* <section id="experiments">
-          <h2>On-going Projects</h2>
-          <p><b>Note:</b> Section is under construction. I am making my site dynamically read it directly from my documentation tool, Notion, which is tracking all my work. I expect this to be up by 25th May, 2023. Apologies for the inconvience!</p>
-          <p>These are my latest projects that I've been working on.</p>
-          <ul>
-            <li>Unified Portfolio manager - personal investments</li>
-            <li>Alfred - the all in 1 butler (using the latest AI algos)</li>
-            <li>Digital Accountal - for daily transactions</li>
-          </ul>
-        </section> */}
+
         <section id="affiliations" className="affiliation">
-          {/* <div className="logo-container"> */}
+
             <h2>Past Affiliations</h2><br/><br/>
 
             <div className="logo-container">
@@ -97,7 +97,6 @@ function AlchemistLab() {
                 <img src={logo} alt={`Company ${index + 1}`} key={index} onContextMenu={(e) => e.preventDefault()} />
               ))}
             </div>
-       
        
         </section>
 
@@ -108,7 +107,6 @@ function AlchemistLab() {
           <ul>
             <li><a href="#about">About</a></li>
             <li><a href="#portfolio">Portfolio</a></li>
-            {/* <li><a href="#experiments">Projects</a></li> */}
             <li><a href="#affiliations">Affiliations</a></li>
 
             <li><span class="construction-icon"><i class="fas fa-tools"></i>Tools</span></li>
